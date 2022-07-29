0bd65dda8b4fd6a4e3e772bf916bfb8bd847e368
maxy <maxyriveyro@gmail.com>
2022-07-29 12:43:49 -0300

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, {useState} from "react"
+
+alert(2);
+function MiApp() {
+const [correoE, setCorreoE] = useState("Correo Vacio");
+function handleCorreo(e) {
+setCorreoE(e.target.value);
+window.miGlobal = e.target.value;
+} 
+debugger 
+  return (
+    <div className="MiApp"> 
+      <div>
+        <h1> Hooks y eventos </h1>
+        <input type="email" value={correoE} onChange={handleCorreo}/>       
+      </div>
+    </div>
+  );
+}
+console.log(window.miGlobal);
+export default MiApp;
